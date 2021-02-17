20bc1135ace8b011366524e30be0e4c4cec0aef4
Faber Sanchez <faber.reach@gmail.com>
2021-02-17 17:14:25 -0500

    Add Redux.Desktop

---
+++ Src/Redux.Desktop/GLFW.cs
@@ -0,0 +1,23 @@
+﻿using Silk.NET.GLFW;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Redux.Desktop
+{
+	internal static unsafe partial class GLFW
+	{
+
+		internal delegate IntPtr glfwGetWin32Window(WindowHandle* window);
+		internal delegate IntPtr glfwGetCocoaWindow(WindowHandle* window);
+
+		internal delegate IntPtr glfwGetX11Window(WindowHandle* window);
+		internal delegate IntPtr glfwGetX11Display();
+
+		internal delegate IntPtr glfwGetWaylandWindow(WindowHandle* window);
+		internal delegate IntPtr glfwGetWaylandDisplay();
+
+	}
+}
--- Src/Redux.Test/Program.cs
+++ Src/Redux.Test/Program.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using Redux.Vulkan;
 using Redux.Core;
-
+using Redux.Desktop;
 namespace Redux.Test
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Adapter adapter = new();
-            Device device = new(adapter);
-            SwapChain swapChain = new(device, new PresentationParameters(120, 100));
+            //Adapter adapter = new();
+            //Device device = new(adapter);
+            //SwapChain swapChain = new(device, new PresentationParameters(120, 100));
+
+
+            Window window = new Window("Redux Engine", 800, 600);
+
+
+            window?.Show();
+            window.RenderLoop(() => { });
 
         }
     }
