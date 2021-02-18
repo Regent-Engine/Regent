2e5a225c1e87e04306baf7ef7e5594335ec65e60
PieroCastillo <pierocastillo2005@gmail.com>
2021-02-18 15:39:22 -0500

    add Redux Property

---
+++ trash/Attributes/ReadonlyReduxPropertyAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Redux.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public sealed class ReadonlyReduxPropertyAttribute : Attribute
+    {
+        public static string ToStringStatic() => "ReadonlyReduxPropertyAttribute";
+    }
+}
